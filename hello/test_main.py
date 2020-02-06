32da22fc7a605cd21180e742090caba08dbe9bfc
Mike Kinney <mike.kinney@gmail.com>
2020-02-06 11:28:07 -0800

    get return code and output

--- hello/test_main.py
+++ hello/test_main.py
@@ -1,16 +1,21 @@
-import os
+import subprocess
+import re
 import pytest
 
 from docopt import docopt
 
 def test_version():
-    exit_status = os.system('python main.py --version')
-    assert exit_status == 0
+    rc, out = subprocess.getstatusoutput('python main.py --version')
+    assert out == '0.0.1'
+    assert re.match(r'[0-9]+\.[0-9]+\.[0-9]', out)
+    assert rc == 0
 
 def test_help():
-    exit_status = os.system('python main.py --help')
-    assert exit_status == 0
+    rc, out = subprocess.getstatusoutput('python main.py --help')
+    assert re.match('Usage', out)
+    assert rc == 0
 
 def test_noname():
-    exit_status = os.system('python main.py')
-    assert exit_status == 0
+    rc, out = subprocess.getstatusoutput('python main.py')
+    assert rc == 0
+    assert out == 'Hello world'
