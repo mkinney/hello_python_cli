b27849a0e1f0e7571718bd449b5e1966d13dd290
Mike Kinney <mike.kinney@gmail.com>
2020-02-14 11:06:23 -0800

    bump version; pull readme from file in setup.py; flake8 fix

--- setup.py
+++ setup.py
@@ -11,13 +11,18 @@ def _get_version():
     return tomlkit.parse(file_contents)['project']['version']
 
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
 setup(
    name='hello_python_cli',
    version=_get_version(),
    description='Simple python hello cli program',
+   long_description=readme,
+   long_description_content_type='text/markdown',
    author='Mike Kinney',
    author_email='mike.kinney@gmail.com',
    packages=['hello_module'],
-   install_requires=[('docopt','tomlkit')],
+   install_requires=[('docopt', 'tomlkit')],
    scripts=['hello'],
 )
