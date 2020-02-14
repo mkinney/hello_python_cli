0b2bd6a6a143a66ad3af43f1e21e45849bf4fff8
Mike Kinney <mike.kinney@gmail.com>
2020-02-14 09:23:19 -0800

    version info in 3 places... not good

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='hello_python_cli',
-   version='0.0.1',
+   version='0.0.2',
    description='Simple python hello cli program',
    author='Mike Kinney',
    author_email='mike.kinney@gmail.com',
